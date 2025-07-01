a6ef16b4000fd025f9d60353d97e25d8035debfc
Josh Harris <josh.harris@stfc.ac.uk>
2025-07-01 11:38:36 +0100

    Formatted for CookieCutter Template

--- control/src/hexitec/adapter.py
+++ control/src/hexitec/adapter.py
@@ -1,1 +1,9 @@
-from base.base_adapter import BaseAdapter
+from .base.base_adapter import BaseAdapter
+from .controller import HexitecController, HexitecError
+
+
+class HexitecAdapter(BaseAdapter):
+    """HEXITEC Adapter class inheriting base adapter functionality."""
+
+    controller_cls = HexitecController
+    error_cls = HexitecError
--- control/src/hexitec/controller.py
+++ control/src/hexitec/controller.py
@@ -1,7 +1,41 @@
+import logging
 from odin.adapters.parameter_tree import ParameterTree, ParameterTreeError
 
-from base.base_controller import BaseController, BaseError
+from .base.base_controller import BaseController, BaseError
 
-class HexitechError(BaseError):
+
+class HexitecError(BaseError):
     """Simple exception class to wrap lower-level exceptions."""
-    pass
+
+
+class HexitecController(BaseController):
+    """Controller class for HEXITEC."""
+
+    def __init__(self, options):
+        self.options = options
+        self.example_param = "Example"
+        self.param_tree = ParameterTree({
+            "example_param": (lambda: self.example_param,  lambda v: setattr(self, 'example_param', v))
+        })
+
+    def initialize(self, adapters):
+        self.adapters = adapters
+        logging.debug(f"Adapters initialized: {list(adapters.keys())}")
+        # Add to param tree if needed post-initialization
+
+    def cleanup(self):
+        logging.info("Cleaning up Controller")
+
+    def get(self, path, with_metadata=False):
+        try:
+            return self.param_tree.get(path, with_metadata)
+        except ParameterTreeError as error:
+            logging.error(error)
+            raise HexitecError(error)
+
+    def set(self, path, data):
+        try:
+            self.param_tree.set(path, data)
+        except ParameterTreeError as error:
+            logging.error(error)
+            raise HexitecError(error)
