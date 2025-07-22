bb93d33f62218d48445a182e60a73e94c5f3553f
Josh Harris <josh.harris@stfc.ac.uk>
2025-07-22 09:02:57 +0100

    Adding initial layout components. Added additional error and recovery states.

--- control/src/hexitec/admonitor/adxdma_state.py
+++ control/src/hexitec/admonitor/adxdma_state.py
@@ -10,117 +10,211 @@ class AdxdmaMonitor(StateMachine):
         registers, looking for data activity and attempting to rebond the AD card. 
     """
     # states
-    monitoring = State('Monitoring', initial=True)
+    initialising = State('Initialising', initial=True)
+    monitoring = State('Monitoring')
     resetting = State('Resetting')
     waiting_for_values = State('WaitingForValues')
-    rebinding = State('Rebinding')
-    waiting_for_bind = State('WaitingForBind')
+    rebonding = State('Rebonding')
+    waiting_for_bond = State('WaitingForbond')
+    error = State('Error')
 
-    # transitions
+    #transistions for init process
+    init_success = initialising.to(monitoring)
+    init_failed = initialising.to(error)
+
+    # transitions for usual workflow
     reset = monitoring.to(resetting)
     start_wait = resetting.to(waiting_for_values)
-    try_bind = waiting_for_values.to(rebinding)
-    wait_bind = rebinding.to(waiting_for_bind)
-    monitor = waiting_for_bind.to(monitoring)
-    restart_reset = waiting_for_bind.to(resetting)
+    try_bond = waiting_for_values.to(rebonding)
+    reset_from_values = waiting_for_values.to(resetting)
+    wait_bond = rebonding.to(waiting_for_bond)
+    monitor = waiting_for_bond.to(monitoring)
+    reset_from_bond = waiting_for_bond.to(resetting)
     loop = monitoring.to(monitoring)
 
-    def __init__(self, controller, check_interval=5, bind_timeout=100, rebind_timeout=100):
+    # error transitions
+    error_from_monitoring = monitoring.to(error)
+    error_from_resetting = resetting.to(error)
+    error_from_waiting = waiting_for_values.to(error)
+    error_from_rebonding = rebonding.to(error)
+    error_from_waiting_bond = waiting_for_bond.to(error)
+
+    # recovery transistions
+    recover = error.to(initialising)
+
+
+    def __init__(self, controller, check_interval=5, bond_timeout=100, rebond_timeout=100):
         self.ctrl = controller
         #self.flags = self.ctrl.ad_flags
         self.check_interval = check_interval
-        self.bind_timeout = bind_timeout
-        self.rebind_timeout = rebind_timeout
+        self.bond_timeout = bond_timeout
+        self.rebond_timeout = rebond_timeout
         self.stat_path = "adxdma/registers/adm_pcie_9v5_stat/"
         self.ctrl_path = "adxdma/registers/adm_pcie_9v5_ctrl/"
         self.proxy = self.ctrl.adapters['proxy']
         self.up = hex(0xfffff)
+        self.cleanup = False
+        self.max_retries = 2
+        self.current_retry = 0
+        self.chan_up = 0
+        self.lane_up = 0
+        self.bonded = False
         # call the StateMachine init to register the states and transitions, 
         # once the rest of the init has finished
         super().__init__()
 
     def _get_status(self):
-        stat = iac_get(self.proxy, self.stat_path, as_dict=True)
-        return (stat['adm_pcie_9v5_stat']['aurora_chan_up']['value'], 
-                stat['adm_pcie_9v5_stat']['aurora_lane_up']['value'])
-    
+        try:
+            stat = iac_get(self.proxy, self.stat_path, as_dict=True)
+            return (stat['adm_pcie_9v5_stat']['aurora_chan_up']['value'], 
+                    stat['adm_pcie_9v5_stat']['aurora_lane_up']['value'])
+        except Exception as e:
+            logging.error(f"Failed to get adxdma status: {e}")
+
+    def _reset_retries(self, value):
+        if self.current_retry >= self.max_retries:
+            self.current_retry = 0
+            self.init_failed()
+        else:
+            logging.error(f"Max retry not met, ignoring reset command")
+
+    def on_enter_initialising(self):
+        """
+            Initialising state action, Initial action for the SM, tests
+            proxy connection to adxdma adapter, transisitons to monitoring
+            if successful else, goes to error state. 
+        """
+        try:
+            if self.current_retry < self.max_retries:
+                stat = iac_get(self.proxy, self.stat_path, as_dict=True)
+                chan = (stat['adm_pcie_9v5_stat']['aurora_chan_up']['value'])
+                self.current_retry = 0
+                self.init_success()
+        except:
+            logging.error(f"Could not read ADXDMA parameters")
+            self.init_failed()
+            self.current_retry += 1
+
     def on_enter_monitoring(self):
         """
-            Initial state action for the SM, monitors the value of channel up and lane up
+            Monitoring state actions, monitors the value of channel up and lane up
             registers, begings state transition to resetting if the values do not
             equal the check value.
         """
-        chan, lane = self._get_status()
-        bound = ((hex(int(chan)) == self.up) & (hex(int(lane)) == self.up))
-        if not bound:
-            logging.info("ADXDMA Lost packet binding, triggering reset")
-            self.reset()
-        else:
-            logging.info("ADXDMA bound to hexitecmhz packets")
-            time.sleep(self.check_interval)
+        try:
+            chan, lane = self._get_status()
+            self.bonded = ((hex(int(chan)) == self.up) & (hex(int(lane)) == self.up))
+            if not self.bonded:
+                logging.info("ADXDMA Lost packet bonding, triggering reset")
+                self.reset()
+            else:
+                logging.info("ADXDMA bonded to hexitecmhz packets")
+                time.sleep(self.check_interval)
+            self.chan_up, self.lane_up = hex(int(chan)), hex(int(lane))
+        except Exception as e:
+            logging.error(f"Error in monitoring: {e}")
+            self.error_from_monitoring()
     
     def on_enter_resetting(self):
         """
             Resetting state aciton, to send reset commands to AD card registers
             to clear the data path, begins state transistion to waiting
         """
-        for register in ['aurora_reset', 'data_path_reset', 'cmac_0_reset']:
-            iac_set(self.proxy, self.ctrl_path + "domain_resets/fields/", register, 1)
-            iac_set(self.proxy, self.ctrl_path + "domain_resets/fields/", register, 0)
-        self.start_wait()
+        try:
+            for register in ['aurora_reset', 'data_path_reset', 'cmac_0_reset']:
+                iac_set(self.proxy, self.ctrl_path + "domain_resets/fields/", register, 1)
+                iac_set(self.proxy, self.ctrl_path + "domain_resets/fields/", register, 0)
+            self.start_wait()
+        except Exception as e:
+            logging.error(f"Error in resetting: {e}")
+            self.error_from_resetting()
 
     def on_enter_waiting_for_values(self):
         """
             Waiting for values state action, reads register values for channel and lane up, 
             looking for them to produce non-zero values indicating data is flowing 
             through the AD data path, if data flow is detected within timeout, begins 
-            transistion to binding state, otherwise returns to monitoring to restart
+            transistion to bonding state, otherwise returns to monitoring to restart
             the process.
         """
-        start = time.time()
-        while time.time() - start < self.bind_timeout and self.ctrl.background_task_en:
-            chan, lane = self._get_status()
-            logging.debug(f"chan:{chan} | lane:{lane}")
-            if ((chan != '0') or (lane != '0')):
-                logging.debug("ADXDMA channels receiving data, re-binding")
-                self.try_bind()
-                return
-            time.sleep(1)
-        logging.warning("No values detected from ADXDMA channels, restarting reset cycle")
-        self.monitor()
+        try:
+            start = time.time()
+            while (time.time() - start < self.bond_timeout and 
+                self.ctrl.executor_threads_en and 
+                    (not self.cleanup)):
+                chan, lane = self._get_status()
+                logging.debug(f"chan:{chan} | lane:{lane}")
+                if ((chan != '0') or (lane != '0')):
+                    logging.debug("ADXDMA channels receiving data, re-bonding")
+                    self.try_bond()
+                    return
+                time.sleep(1)
+            logging.error("No values detected from ADXDMA channels, restarting reset cycle")
+            self.reset_from_values()
+        except Exception as e:
+            logging.error(f"Error in waiting for values: {e}")
+            self.error_from_waiting()
 
-    def on_enter_rebinding(self):
+    def on_enter_rebonding(self):
         """
-            Rebinding state action, tells the LOKI controller to send rebonding 
-            packets to the AD card. Transistions to waiting for bind state.
-        """
-        iac_set(
-            self.proxy,
-            'loki/application/system_state',
-            {'ASIC_REBOND': True}
-        )
-        logging.info("ASIC_REBOND command sent, waiting for binding to complete")
-        self.wait_bind()
+            Rebonding state action, tells the LOKI controller to stop sending data
+            and to then send rebonding packets to the AD card. Transistions to waiting 
+            for bond state.
+        """ 
+        try:   
+            iac_set(
+                self.proxy,
+                'loki/application/system_state',
+                {'SYNC': False}
+            )
+            iac_set(
+                self.proxy,
+                'loki/application/system_state',
+                {'ASIC_REBOND': True}
+            )
+            logging.info("ASIC_REBOND command sent, waiting for bonding to complete")
+            self.wait_bond()
+            return
+        except Exception as e:
+            logging.error(f"Error from rebonding: {e}")
+            self.error_from_rebonding()
 
-    def on_enter_waiting_for_bind(self):
+    def on_enter_waiting_for_bond(self):
         """
             Waiting for bon action, monitors the value of the channel and lane
             up registers within a timeout, return to monitoring state if they both
             equal the check value, else restart the resetting process and transition 
             back to resetting if AD card doesnt bond within timeout.
         """
-        start = time.time()
-        while time.time() - start < self.rebind_timeout and self.ctrl.background_task_en:
-            chan, lane = self._get_status()
-            bound = ((hex(int(chan)) == self.up) & (hex(int(lane)) == self.up))
-            if bound:
-                logging.info("ADXDMA successfully bound after rebind")
-                self.monitor()
-                return
-            time.sleep(1)
-        
-        logging.warning("Rebind timeout - restarting reset cycle")
-        self.restart_reset()
+        try:
+            start = time.time()
+            while (time.time() - start < self.bond_timeout and 
+                self.ctrl.executor_threads_en and 
+                    (not self.cleanup)):
+                chan, lane = self._get_status()
+                self.bonded = ((hex(int(chan)) == self.up) & (hex(int(lane)) == self.up))
+                if self.bonded:
+                    logging.info("ADXDMA successfully bonded after rebond")
+                    # Tell loki to start sending data after bonding is complete
+                    iac_set(
+                        self.proxy,
+                        'loki/application/system_state',
+                        {'SYNC': True}
+                    ) 
+                    self.monitor()
+                    return
+                time.sleep(1)
+            
+            logging.warning("Rebond timeout - restarting reset cycle")
+            self.reset_from_bond()
+        except Exception as e:
+            logging.error(f"Error from waiting for bond: {e}")
+            self.error_from_waiting_bond()
 
-
-        
+    def on_enter_error(self):
+        """
+            Error state action, waits for a period then attempts recovery
+            by going through the initialisation process
+        """
+        logging.info("Attempting recovery through initialisation")
+        self.recover()
--- control/src/hexitec/controller.py
+++ control/src/hexitec/controller.py
@@ -16,15 +16,14 @@ class HexitecError(BaseError):
 
 class HexitecController(BaseController):
     """Controller class for HEXITEC."""
-    executor = ThreadPoolExecutor(max_workers=1)
+    executor = ThreadPoolExecutor(max_workers=2)
 
     def __init__(self, options):
         self.options = options
-        self.background_task_en = True
+        self.executor_threads_en = True
         self.ad_flags = AdxdmaFlags()
         self.adxdma_monitor = None
-        self.param_tree = ParameterTree({
-        })
+        self.param_tree = None
 
     def initialize(self, adapters):
         self.adapters = adapters
@@ -32,16 +31,71 @@ class HexitecController(BaseController):
 
         #add self.options here
         try:
-            self.adxdma_monitor = AdxdmaMonitor(controller=self, check_interval=5, bind_timeout=100)
+            self.adxdma_monitor = AdxdmaMonitor(controller=self, check_interval=5, bond_timeout=100)
         except Exception as e:
             logging.debug(f"Error: {e}")
+
+        try:
+            if self.adxdma_monitor:
+                self.adxdma_tree = ParameterTree({
+                    'current_retry': (lambda: self.adxdma_monitor.current_retry, None),
+                    'max_retries': (lambda: self.adxdma_monitor.max_retries, None),
+                    'reset_retries': (lambda: None, self.adxdma_monitor._reset_retries),
+                    'chan_up': (lambda: self.adxdma_monitor.chan_up, None),
+                    'lane_up': (lambda: self.adxdma_monitor.lane_up, None),
+                    'bonded': (lambda: self.adxdma_monitor.bonded, None),
+                    'current_state': (lambda: self.adxdma_monitor.current_state.name, None),
+                    'all_states': (lambda: [state.name for state in self.adxdma_monitor.states], None),
+                    'transition_details': (lambda: [
+                        {
+                            'name': t.event,
+                            'from': t.source.id,
+                            'to': t.target.id
+                        }
+                        for state in self.adxdma_monitor.states_map.values()
+                        for t in state.transitions
+                    ], None),
+                    'available_from_current': (lambda: [
+                        {
+                            'name': t.event,
+                            'to': t.target.id
+                        }
+                        for t in self.adxdma_monitor.current_state.transitions
+                    ] if self.adxdma_monitor.current_state else [], None)
+                })
+            else:
+                self.adxdma_tree = ParameterTree({
+                    'current_state': (lambda: "Not Available", None),
+                    'all_states': (lambda: [], None),
+                    'transition_details': (lambda: [], None),
+                    'available_from_current': (lambda: [], None)
+                })
+        except Exception as e:
+            logging.debug(f"Error: {e}")
+
+        # all_transitions = [
+        #     t
+        #     for state in self.adxdma_monitor.states_map.values()     
+        #     for t in state.transitions
+        # ]
+
+
+        # logging.debug(f"{all_transitions}")
+        # for t in all_transitions:
+        #     logging.debug(f"{t.source.id} -> {t.target.id}")
+
+        self.param_tree = ParameterTree({
+            'adxdma_monitor': self.adxdma_tree
+        })
         self.background_task()
+        self.admonitor_task()
 
     def cleanup(self):
         """Cleanly shutdown adapter services"""
         logging.info("Cleaning up Controller")
-        logging.info('Stopping background task')
-        self.background_task_en = False
+        self.executor_threads_en = False
+        if self.adxdma_monitor:
+            self.adxdma_monitor.cleanup = True
 
     def get(self, path, with_metadata=False):
         try:
@@ -60,10 +114,19 @@ class HexitecController(BaseController):
     @run_on_executor
     def background_task(self):
         """"""
-        logging.debug("calling background task")
-        while self.background_task_en:
+        return
+        while self.executor_threads_en:
             time.sleep(1)
-            try:
-                self.adxdma_monitor.loop()
-            except Exception as e:
-                logging.error(f"ADXDMA monitor error:{e}")
+
+    @run_on_executor
+    def admonitor_task(self):
+        """"""
+        while self.executor_threads_en:
+            time.sleep(1)
+            # only cll loop if the statemachine created successfully
+            if self.adxdma_monitor:
+                try:
+                    if self.adxdma_monitor.current_state.name == 'Monitoring':
+                        self.adxdma_monitor.loop()
+                except Exception as e:
+                    logging.error(f"ADXDMA monitor error:{e}")
