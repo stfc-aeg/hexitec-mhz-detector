90c985af74c27c176770af9c6f326ebe850b459d
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2025-10-13 11:38:57 +0100

    #7 Add Power&Env controls + utils

---
+++ control/web/static/hexitec-react/src/utils.js
@@ -0,0 +1,15 @@
+// Utility function that checks if a value is null or undefined. Returns 'null' if yes, value if no
+export const checkNull = (val) => val === null || val === undefined ? 'null' : val.toFixed(4);
+
+export const checkNullNoDp = (val) => val === null || val === undefined ? 'null' : val;
+
+export const floatingLabelStyle = {
+    border: '1px solid lightblue',
+    backgroundColor: '#e0f7ff',
+    borderRadius: '0.375rem'
+}
+
+export const floatingInputStyle = {
+  border: "1px solid #ced4da", // thin Bootstrap-like grey border
+  borderRadius: "0.375rem",    // Bootstrap default rounded corners
+};
