56436189eb887dfb47798374b23319a208c84205
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2026-01-27 10:31:30 +0000

    #12 acq initialize and change_bin_mode

--- control/src/hexitec/acquisition/controller.py
+++ control/src/hexitec/acquisition/controller.py
@@ -44,37 +44,31 @@ class AcquisitionController(BaseController):
         :param adapters: dictionary of adapter instances keyed by name
         """
         self.adapters = adapters
-
-        # hexitec, proxy, registerAccessor, munir, liveview, histogram
-        # cast these in typing to force typing to accept the narrow XYController vs BaseController
-        if 'histogram' in self.adapters:
-            self.histogrammer = cast(HistogramController, self.adapters['histogram'].controller)
-        else:
-            raise AcquisitionError("Histogrammer adapter not found during AcquisitionController initialisation.")
         
-        if 'liveview' in self.adapters:
-            self.liveview = cast(HistogramLiveViewController, self.adapters['liveview'].controller)
-        else:
-            raise AcquisitionError("Liveview adapter not found during AcquisitionController initialisation.")
-
-        if 'munir' in self.adapters:
-            self.munir = cast(MunirFpController, self.adapters['munir'].controller)
-            if self.munir_subsystem in self.munir.munir_managers.keys():
-                self.munir_hexitec = self.munir.munir_managers[self.munir_subsystem]  # This subsystem name is known and has 1 endpoint
-            else:
-                raise AcquisitionError(f"Could not find munir subsystem {self.munir_subsystem} as expected. Please check configuration.")
-        else:
-            raise AcquisitionError("Munir adapter not found during AcquisitionController initialisation.")
-
-        if 'proxy' in self.adapters:
-            self.proxy: ProxyAdapter = self.adapters['proxy']
-        else:
-            raise AcquisitionError("Proxy adapter not found during AcquisitionController initialisation.")
+        # Verify all required adapters are present
+        required_adapters = ['histogram', 'liveview', 'munir', 'proxy', 'hexitec']
+        missing = [name for name in required_adapters if name not in adapters]
+        if missing:
+            raise AcquisitionError(f"Missing required adapters: {', '.join(missing)}")
         
-        if 'hexitec' in self.adapters:
-            self.hexitec = cast(HexitecController, self.adapters['hexitec'].controller)
-        else:
-            raise AcquisitionError("Hexitec adapter not found during AcquisitionController initialisation.")
+        # Cast and store adapter controllers
+        self.histogrammer = cast(HistogramController, adapters['histogram'].controller)
+        self.liveview = cast(HistogramLiveViewController, adapters['liveview'].controller)
+        self.munir = cast(MunirFpController, adapters['munir'].controller)
+        self.proxy = cast(ProxyAdapter, adapters['proxy'])
+        self.hexitec = cast(HexitecController, adapters['hexitec'].controller)
+        
+        # Verify munir subsystem exists
+        if self.munir_subsystem not in self.munir.munir_managers:
+            raise AcquisitionError(
+                f"Could not find munir subsystem '{self.munir_subsystem}' in available managers: "
+                f"{list(self.munir.munir_managers.keys())}"
+            )
+        self.munir_hexitec = self.munir.munir_managers[self.munir_subsystem]
+
+        # Connect histogrammer and setup UDP
+        self.histogrammer.setConnect(True)
+        self.histogrammer.setupUDP()
 
     def _start_preview(self):
         pass
@@ -88,11 +82,11 @@ class AcquisitionController(BaseController):
         :param bin_mode: string representing the operating mode, typically a number of bins. See allowed values metadata
         """
         munir_odindata_controller = self.munir_hexitec.odin_data_instances[0]
-        was_executing = False
 
         # Done this way for futureproofing, e.g. mapped modes might be 'histogram_1024_map' and need different handling
         match bin_mode:
-            case 'histogram_128', 'histogram_256', 'histogram_512', 'histogram_1024', 'histogram_2048', 'histogram_4096':
+            case ('histogram_128' | 'histogram_256' | 'histogram_512' | 'histogram_1024' | 'histogram_2048' | 'histogram_4096'):
+                self.bin_mode = bin_mode
                 hist_value = bin_mode.split('_')[-1]
                 depth = int(hist_value)
                 hist_mode="numBins"
@@ -110,43 +104,53 @@ class AcquisitionController(BaseController):
         self.histogrammer.setRun(False)
 
         # Change via histogrammer
-        self.histogrammer.setHistFormat(hist_mode, hist_value)
+        self.histogrammer.setHistFormat(setting=hist_mode, value=hist_value)
+
+        not_cfg={
+            'HexitecMhz': {
+                'mode': 'histogram_2048'
+            }, 
+            'hdf': {
+                'dataset': {
+                    'dummy': {
+                            'datatype': 'uint32', 
+                            'dims': [80, 80, 2048], 
+                            'compression': 'none'
+                        }
+                    }, 
+                'file': {
+                    'path': '/tmp/'
+                }
+            }
+        }
 
         # Change in odin data
         cfg = {
-        "params": {
             "HexitecMhz": {
-                "mode": bin_mode
+                "mode": self.bin_mode
             },
-        "hdf":{
-            "dataset": {
-                    "dummy": {
-                        "datatype": "uint32",
-                        "dims": [80, 80, depth],
-                        "compression": "none"
-                    }
-                },
-                "write": False,
-            }
-        }}
-        munir_odindata_controller._send_receive('cmd', 'config', params=cfg)
+            "hdf":{
+                "dataset": {
+                        "dummy": {
+                            "datatype": "uint32",
+                            "dims": [80, 80, depth],
+                            "compression": "none"
+                        }
+                    },
+                    "write": False,
+                }
+        }
+        response = munir_odindata_controller.set_config(cfg)
+        logging.warning(f"Odin-data config response: {response}")
 
         # Change in liveview
         self.liveview.set_num_bins(depth, self.liveview.processors[0])  # Only anticipate one endpoint
-        self.liveview.processors[0]
 
         # Restart liveviewing
         self.histogrammer.setRun(True)
 
         if was_executing:
             self.munir.set_execute('hexitec_mhz', True)
-
-        # Stop odin-data
-        # Stop liveview via histogrammer
-        # Change via histogrammer
-        # Change in odin-data (filewriter and mhz plugins)
-        # Change in liveview (and make sure that changes dimensions/energy_range)
-        # Restart liveview and then odin-data
 
     def _start_acquisition(self):
         # Check histogrammer details are sensible
@@ -161,11 +165,6 @@ class AcquisitionController(BaseController):
         # Tell odin data to stop
         # Restart preview? Perhaps should not turn on automatically, could be optional
         pass
-
-    def cleanup(self):
-        """Clean up controller resources."""
-        logging.debug("Cleaning up AcquisitionController")
-
 
     def get(self, path, with_metadata=False):
         """Get parameter data from controller."""
@@ -182,3 +181,7 @@ class AcquisitionController(BaseController):
         except ParameterTreeError as error:
             logging.error("Error setting parameter: %s", error)
             raise AcquisitionError(str(error))
+        
+    def cleanup(self):
+        """Clean up controller resources."""
+        logging.debug("Cleaning up AcquisitionController")
